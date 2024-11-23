33e54cb017b1ed4e27a28e0cb3e328c3e10de84d
BHUPATHI-LAKSHMI-PRAVALLIKA <77114989+BHUPATHI-LAKSHMI-PRAVALLIKA@users.noreply.github.com>
2024-11-22 21:01:28 -0600

    Updated RegisterActivity.java

--- app/src/main/java/com/example/notetakerapplication/RegisterActivity.java
+++ app/src/main/java/com/example/notetakerapplication/RegisterActivity.java
@@ -3,8 +3,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity; 
+
 public class RegisterActivity extends AppCompatActivity {
+    
     @Override
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registrationlayout);
@@ -12,6 +15,7 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton.setOnClickListener(v -> {
             Intent in = new Intent(RegisterActivity.this, LoginActivity.class);
             startActivity(in);
+            
         });
     }
 }
