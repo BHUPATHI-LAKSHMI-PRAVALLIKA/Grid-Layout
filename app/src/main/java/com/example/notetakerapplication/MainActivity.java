41b9a25206026f08df57aa2498f797a69215c4b4
BHUPATHI-LAKSHMI-PRAVALLIKA <77114989+BHUPATHI-LAKSHMI-PRAVALLIKA@users.noreply.github.com>
2024-11-22 21:00:04 -0600

    Updated MainActivity.java

--- app/src/main/java/com/example/notetakerapplication/MainActivity.java
+++ app/src/main/java/com/example/notetakerapplication/MainActivity.java
@@ -7,15 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 public class MainActivity extends AppCompatActivity {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button addNoteButton = findViewById(R.id.addNoteButton);
         addNoteButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, AddEditNoteActivity.class);
             startActivity(intent);
+            
         });
     }
 }
