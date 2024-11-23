3fbc8a69327ecb437a1b7e93a3cbdcf1f06c214c
BHUPATHI-LAKSHMI-PRAVALLIKA <77114989+BHUPATHI-LAKSHMI-PRAVALLIKA@users.noreply.github.com>
2024-11-22 21:00:46 -0600

    Updated MyApplication.java

--- app/src/main/java/com/example/notetakerapplication/MyApplication.java
+++ app/src/main/java/com/example/notetakerapplication/MyApplication.java
@@ -1,13 +1,16 @@
 package com.app.notes;
 
 import android.app.Application;
+
 import android.util.Log;
 
 import com.google.firebase.FirebaseApp;
 
 public class MyApplication extends Application {
+    
     @Override
     public void onCreate() {
+        
         super.onCreate();
 
 
