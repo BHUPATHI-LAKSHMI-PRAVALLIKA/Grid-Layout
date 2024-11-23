ebd851b9ee818d564b9c02f45ed0cd9c9494c44a
BHUPATHI-LAKSHMI-PRAVALLIKA <77114989+BHUPATHI-LAKSHMI-PRAVALLIKA@users.noreply.github.com>
2024-11-22 20:56:17 -0600

    Updated login activity

--- app/src/main/java/com/example/notetakerapplication/LoginActivity.java
+++ app/src/main/java/com/example/notetakerapplication/LoginActivity.java
@@ -3,8 +3,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+
 public class LoginActivity extends AppCompatActivity {
     @Override
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginlayout);
@@ -14,9 +16,11 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
         });
+        
         registerButton.setOnClickListener(v -> {
             Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
             startActivity(intent);
+            
         });
     }
 }
